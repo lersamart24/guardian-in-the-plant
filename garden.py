fbbee6fa1429bfbf742bab34076a388d8e68a4ee
lersamart24 <sasawatgin.1@gmail.com>
2026-03-31 18:29:52 +0700

    Update garden.py

--- garden.py
+++ garden.py
@@ -1,37 +1,38 @@
 import time
 backpack = []
 live = []
-print("You wake and go outside to water your plant")
-time.sleep(0.6)
-print(" ")
-print("But when he try to water your sun flower plant there was no water left")
-time.sleep(0.6)
-print(" ")
-while True:
-    first_step = input("What will you do (go find water/just give up/get a new plant/water your plant with tea)").lower()
-    if first_step == "just give up":
-        print("you give up and your plant die try again")
-    
-    elif first_step == "get a new plant":
-        print("you get a new plant can talk and it talk about how it going to take over the world")
-        time.sleep(0.5)
-        print(" ")
-        print("so you just water it with pool water try again")
-    
-    
-    elif first_step == "water your plant with tea":
-        print("you water your plant with tea and it die.")
-        time.sleep(0.5)
-        print(" ")
-        print("i don't know what are you thinking with you pick this try again")
-        
-    elif first_step == "go find water":
-        print("you going to start the new adventure of your own")
-        
-        break
+import turtle
 
-    else:
-        print("you miss spell try again")
+
+ def sun_flower():
+     while True:
+             first_step = input("What will you do (go find water/just give up/get a new plant/water your plant with tea)").lower()
+             if first_step == "just give up":
+                 print("you give up and your plant die try again")
+                
+        
+            elif first_step == "get a new plant":
+                print("you get a new plant can talk and it talk about how it going to take over the world")
+                time.sleep(0.5)
+                print(" ")
+                print("so you just water it with pool water try again")
+                
+        
+            elif first_step == "water your plant with tea":
+                 print("you water your plant with tea and it die.")
+                 time.sleep(0.5)
+                 print(" ")
+                 print("i don't know what are you thinking with you pick this try again")
+               
+             elif first_step == "go find water":
+                 print("you going to start the new adventure of your own")
+                 shovel()
+                 break
+                
+    
+            else:
+                print("you miss spell try again")
+                
 
 
 
@@ -42,13 +43,13 @@ while True:
 
 
 def shovel():
-    shovel = input("you hit somthing when you trying to find water(pick it/leave it there)").lower()
-        if shovel == "pick it":
-            print("you pick it and you keep going")
     
-        elif shovel == "leave it there":
+    shovel = input("you hit somthing when you trying to find water(pick it/leave it there)").lower()
+    if shovel == "pick it":
+             print("you pick it and you keep going")
+    
+    elif shovel == "leave it there":
             print("you miss on somthing very cool")
     
-        else:
-            print("you miss spell")
-    print("")
+    else:
+             print("you miss spell")
