2a555a300199d9501ff330d6d4eb02066e742c70
lersamart24 <sasawatgin.1@gmail.com>
2026-04-21 18:32:08 +0700

    Update garden.py

--- garden.py
+++ garden.py
@@ -1,79 +1,104 @@
-
 import time
-backpack = []
-live = []
 
-def sun_flower():
-    print("right now the world is in danger because there was a outbreak in the biohazard ")
+backpack = []
+
+
+def intro():
+    print("right now the world is in danger because there was a outbreak in the biohazard.")
     time.sleep(0.6)
-    print(" ")
-    print("The outbreak as mutate the plant and human the plant now is 10x stronger and the human infect by the zoonotic bridge rafflesia(ZBR)")
+    print("the outbreak has mutate the plant and human the plant now is 10x stronger")
+    print("humans are infected by the zoonotic bridge rafflesia (ZBR)")
+
+    alive = True
+
     while True:
-        first_step = input("what will you do(save the world/do nothing): ").lower()
+        first_step = input("everything start at 12:00 PM what will you do (save the world / do nothing): ").lower()
 
         if first_step == "do nothing":
             print("you got the bad ending every one die")
+            alive = False
             break
 
- 
-
         elif first_step == "save the world":
-            print("You explore trying to find somthing that will help you with your adventure")
-            shovel()
+            print("you start your journey...")
+            alive = True
             break
 
         else:
             print("you miss spell try again")
 
-#NEW STUFF LATER IN FIXING MODE
+    return alive
+
+
 def shovel():
     while True:
-        choice = input("while your trying to find somthing you found a Ruellia tuberosa(pick/leave)").lower()
-            
+        choice = input("you found a Ruellia tuberosa (pick/leave): ").lower()
 
         if choice == "pick":
-            print("you pick it and it's an Ruellia tuberosa, you store it in your backpack and  keep going")
+            print("you pick it and store it in your backpack")
             backpack.append("Ruellia tuberosa")
             break
 
-        
         elif choice == "leave":
-            print("you miss on a very cool looking thing try")
+            print("you miss on a very cool thing")
             break
 
         else:
             print("you miss spell")
 
 
+def biohazard_scene():
+    print("you pass by a biohazard")
+
+    while True:
+        plant = input("do you want to go in (yes/no): ").lower()
+
+        if plant == "yes":
+            print("you found an apple tree with a rafflesia around it")
+            start()
+            break
+
+        elif plant == "no":
+            print("you step on a sundew plant it eat you try again")
+
+        else:
+            print("i didn't understand")
+
+
 def start():
-    print("beside that you found a sheet of paper")
-    time.sleep(0.6)
-    print(" ")
-    print("it say (Since the breakout i have been search and found out) ")
-    time.sleep(0.6)
-    print(" ")
-    print("What the only cure to the ZBR is the golden apple but the rafflesia going to kill it in 2 day")
-    time.sleep(0.6)
-    print(" ")
-    print("the only rafflesia weakness is fire ")
-    print(" ")
+    print("you find a sheet of paper...")
     time.sleep(0.6)
 
+    print("it say the only cure to ZBR is the golden apple")
+    time.sleep(0.6)
 
-sun_flower()
+    print("the rafflesia will destroy it in 2 day")
+    time.sleep(0.6)
 
-print("you pass by a biohazrd")
-while True:
-    plant = input("Do you want to go and check what in it (yes/no): ").lower()
-    
-    if plant == "yes":
-                print("You go in to check and found a apple tree with a rafflesia in wrap around the trunk")
-                start()
-                break
-        
-    
-    elif plant == "no":
-        print("You didn't go in and you step on a sundew plant it eat you try again ")
-        
-    else:
-        print("I didn't understand, please type yes or no.")
+    print("the only rafflesia weakness is fire")
+
+
+def fight_scene():
+    while True:
+        pepsii = input("what should you do 1(fight it) 2(run): ")
+
+        if pepsii == "1":
+            print("you try to fight it but you are too weak and die")
+            
+
+        elif pepsii == "2":
+            print("you run away but find no survivors")
+            break
+
+        else:
+            print("i didn't understand try again")
+
+
+# ---- GAME START ----
+
+alive = intro()
+
+if alive:
+    shovel()
+    biohazard_scene()
+    fight_scene()
